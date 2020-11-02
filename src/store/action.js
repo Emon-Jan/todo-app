2e911e3a90bc192a6d9016d9102d9912ef411c2b
Muktadir Imam Jan <muktadirimam@gmail.com>
2020-11-02 20:07:03 +0600

    reducer action types

---
+++ src/store/action.js
@@ -0,0 +1,9 @@
+export const CHANGE_TODO = "CHANGE_TODO";
+export const CHANGE_EDIT_TODO = "CHANGE_EDIT_TODO";
+export const HOVER_STATE = "HOVER_STATE";
+export const CREATE_TODO = "CREATE_TODO";
+export const EDIT_TODO = "EDIT_TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const CHECK_TODO = "CHECK_TODO";
+export const CLEAR_COMPLETE_TODO = "CLEAR_COMPLETE_TODO";
